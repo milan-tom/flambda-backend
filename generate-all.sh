96c1a4f6ba9510e2925a28c777d7954e255a7b9f
Milan Tom <milant076@gmail.com>
2024-09-02 14:11:44 +0100

    only do file

--- generate-all.sh
+++ generate-all.sh
@@ -4,32 +4,42 @@ original=false
 main=false
 latest=true
 
-if [ "$original" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-original.csv ""
-fi
-if [ "$main" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-main.csv "regalloc-param=BLOCK_TEMPORARIES:on"
-fi
-if [ "$latest" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-latest.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+file=true
+func=false
+block=false
+
+if [ "$file" = true ]; then
+  if [ "$original" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-original.csv ""
+  fi
+  if [ "$main" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-main.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+  fi
+  if [ "$latest" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-latest.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+  fi
 fi
 
-if [ "$original" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-original-func.csv "granularity=func"
-fi
-if [ "$main" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-main-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
-fi
-if [ "$latest" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-latest-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
+if [ "$func" = true ]; then
+  if [ "$original" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-original-func.csv "granularity=func"
+  fi
+  if [ "$main" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-main-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
+  fi
+  if [ "$latest" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-latest-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
+  fi
 fi
 
-if [ "$original" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-original-block.csv "granularity=block"
-fi
-if [ "$main" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-main-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
-fi
-if [ "$latest" = true ]; then
-  ./scripts/profile-compiler-build.sh summary-latest-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
+if [ "$block" = true ]; then
+  if [ "$original" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-original-block.csv "granularity=block"
+  fi
+  if [ "$main" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-main-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
+  fi
+  if [ "$latest" = true ]; then
+    ./scripts/profile-compiler-build.sh summary-latest-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
+  fi
 fi
