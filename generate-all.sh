4aea5918d3e3cb65a6b52cab8178dc69cede9d37
Milan Tom <milant076@gmail.com>
2024-09-02 13:14:53 +0100

    only update latest

--- generate-all.sh
+++ generate-all.sh
@@ -1,9 +1,35 @@
 set -e -u -o pipefail
 
-./scripts/profile-compiler-build.sh summary-original.csv ""
-./scripts/profile-compiler-build.sh summary-latest.csv "regalloc-param=BLOCK_TEMPORARIES:on"
-./scripts/profile-compiler-build.sh summary-original-func.csv "granularity=func"
-./scripts/profile-compiler-build.sh summary-latest-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
-./scripts/profile-compiler-build.sh summary-original-block.csv "granularity=block"
-./scripts/profile-compiler-build.sh summary-latest-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
-./scripts/profile-compiler-build.sh temp-testing.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+original=false
+main=false
+latest=true
+
+if [ "$original" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-original.csv ""
+fi
+if [ "$main" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-main.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+fi
+if [ "$latest" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-latest.csv "regalloc-param=BLOCK_TEMPORARIES:on"
+fi
+
+if [ "$original" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-original-func.csv "granularity=func"
+fi
+if [ "$main" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-main-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
+fi
+if [ "$latest" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-latest-func.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=func"
+fi
+
+if [ "$original" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-original-block.csv "granularity=block"
+fi
+if [ "$main" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-main-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
+fi
+if [ "$latest" = true ]; then
+  ./scripts/profile-compiler-build.sh summary-latest-block.csv "regalloc-param=BLOCK_TEMPORARIES:on,granularity=block"
+fi
